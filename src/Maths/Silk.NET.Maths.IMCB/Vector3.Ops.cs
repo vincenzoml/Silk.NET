362909229cc738daabac7a05400a93f989c092a7
Dylan Perks <dmp9biz@gmail.com>
2020-11-07 20:30:38 +0000

    Implement Vector3, add pending Vector3.Ops

--- src/Maths/Silk.NET.Maths.IMCB/Vector3.Ops.cs
+++ src/Maths/Silk.NET.Maths.IMCB/Vector3.Ops.cs
@@ -3,10 +3,187 @@
 // You may modify and distribute Silk.NET under the terms
 // of the MIT license. See the LICENSE file for details.
 
+using System;
+
 namespace Silk.NET.Maths
 {
     public static class Vector3
     {
-        
+        public static Vector3<T> Add<T>(Vector3<T> a, Vector3<T> b) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Subtract<T>(Vector3<T> a, Vector3<T> b) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Multiply<T>(Vector3<T> vector, T scale) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Multiply<T>(Vector3<T> vector, Vector3<T> scale) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Divide<T>(Vector3<T> vector, T scale) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Divide<T>(Vector3<T> vector, Vector3<T> scale) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> ComponentMin<T>(Vector3<T> a, Vector3<T> b) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> ComponentMax<T>(Vector3<T> a, Vector3<T> b) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> MagnitudeMin<T>(Vector3<T> left, Vector3<T> right) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> MagnitudeMax<T>(Vector3<T> left, Vector3<T> right) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Clamp<T>(Vector3<T> vec, Vector3<T> min, Vector3<T> max) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static T Distance<T>(Vector3<T> vec1, Vector3<T> vec2) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static T DistanceSquared<T>(Vector3<T> vec1, Vector3<T> vec2) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Normalize<T>(Vector3<T> vec) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static T Dot<T>(Vector3<T> left, Vector3<T> right) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Cross<T>(Vector3<T> left, Vector3<T> right) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Lerp<T>(Vector3<T> a, Vector3<T> b, T blend) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> BaryCentric<T>(Vector3<T> a, Vector3<T> b, Vector3<T> c, T u, T v) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> TransformVector<T>(Vector3<T> vec, Matrix4<T> mat) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> TransformNormal<T>(Vector3<T> norm, Matrix4<T> mat) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> TransformNormalInverse<T>(Vector3<T> norm, Matrix4<T> invMat) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> TransformPosition<T>(Vector3<T> pos, Matrix4<T> mat) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Transform<T>(Vector3<T> vec, Matrix3<T> mat) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> TransformColumn<T>(Vector3<T> vec, Matrix3<T> mat) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Transform<T>(Vector3<T> vec, Quaternion<T> quat) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Transform<T>(Matrix3<T> mat, Vector3<T> vec) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> TransformColumn<T>(Matrix3<T> mat, Vector3<T> vec) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> TransformPerspective<T>(Vector3<T> vec, Matrix4<T> mat) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static T CalculateAngle<T>(Vector3<T> first, Vector3<T> second) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Project<T>
+        (
+            Vector3<T> vector,
+            T x,
+            T y,
+            T width,
+            T height,
+            T minZ,
+            T maxZ,
+            Matrix4<T> worldViewProjection
+        ) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Unproject<T>
+        (
+            Vector3<T> vector,
+            T x,
+            T y,
+            T width,
+            T height,
+            T minZ,
+            T maxZ,
+            Matrix4<T> inverseWorldViewProjection
+        ) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector3<T> Negate<T>(Vector3<T> vec) where T : unmanaged => Multiply(vec, Vector3<T>.MinusOne);
     }
 }
--- src/Maths/Silk.NET.Maths.IMCB/Vector3.cs
+++ src/Maths/Silk.NET.Maths.IMCB/Vector3.cs
@@ -4,6 +4,8 @@
 // of the MIT license. See the LICENSE file for details.
 
 using System;
+using static Silk.NET.Maths.Helper;
+using static Silk.NET.Maths.Scalar;
 
 namespace Silk.NET.Maths
 {
@@ -15,10 +17,10 @@ namespace Silk.NET.Maths
         public static readonly Vector3<T> UnitZ = new Vector3<T>(Scalar<T>.Zero, Scalar<T>.Zero, Scalar<T>.One);
         public static readonly Vector3<T> Zero = new Vector3<T>(Scalar<T>.Zero);
         public static readonly Vector3<T> One = new Vector3<T>(Scalar<T>.One);
+        public static readonly Vector3<T> MinusOne = new Vector3<T>(Negate(Scalar<T>.One));
         public static readonly Vector3<T> PositiveInfinity = new Vector3<T>(Scalar<T>.PositiveInfinity);
         public static readonly Vector3<T> NegativeInfinity = new Vector3<T>(Scalar<T>.NegativeInfinity);
-        public static readonly int SizeInBytes;
-        private static readonly string ListSeparator;
+        public static readonly unsafe int SizeInBytes = sizeof(Vector3<T>);
         public T X;
         public T Y;
         public T Z;
@@ -32,492 +34,101 @@ namespace Silk.NET.Maths
 
         public Vector3(T x, T y, T z)
         {
-            throw new NotImplementedException();
+            X = x;
+            Y = y;
+            Z = z;
         }
 
         public Vector3(Vector2<T> v)
         {
-            throw new NotImplementedException();
+            X = v.X;
+            Y = v.Y;
+            Z = Scalar<T>.Zero;
         }
 
         public Vector3(Vector3<T> v)
         {
-            throw new NotImplementedException();
+            X = v.X;
+            Y = v.Y;
+            Z = v.Z;
         }
 
         public Vector3(Vector4<T> v)
         {
-            throw new NotImplementedException();
+            X = v.X;
+            Y = v.Y;
+            Z = v.Z;
         }
 
         public T this[int index]
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
-        }
-
-        public T Length => throw new NotImplementedException();
-
-        public T LengthFast => throw new NotImplementedException();
-
-        public T LengthSquared => throw new NotImplementedException();
-
-        public Vector3<T> Normalized()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Normalize()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void NormalizeFast()
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Add(Vector3<T> a, Vector3<T> b)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Add(ref Vector3<T> a, ref Vector3<T> b, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Subtract(Vector3<T> a, Vector3<T> b)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Subtract(ref Vector3<T> a, ref Vector3<T> b, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Multiply(Vector3<T> vector, T scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Multiply(ref Vector3<T> vector, T scale, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Multiply(Vector3<T> vector, Vector3<T> scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Multiply(ref Vector3<T> vector, ref Vector3<T> scale, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Divide(Vector3<T> vector, T scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Divide(ref Vector3<T> vector, T scale, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Divide(Vector3<T> vector, Vector3<T> scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Divide(ref Vector3<T> vector, ref Vector3<T> scale, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> ComponentMin(Vector3<T> a, Vector3<T> b)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void ComponentMin(ref Vector3<T> a, ref Vector3<T> b, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> ComponentMax(Vector3<T> a, Vector3<T> b)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void ComponentMax(ref Vector3<T> a, ref Vector3<T> b, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> MagnitudeMin(Vector3<T> left, Vector3<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void MagnitudeMin(ref Vector3<T> left, ref Vector3<T> right, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> MagnitudeMax(Vector3<T> left, Vector3<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void MagnitudeMax(ref Vector3<T> left, ref Vector3<T> right, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Clamp(Vector3<T> vec, Vector3<T> min, Vector3<T> max)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Clamp
-        (
-            ref Vector3<T> vec,
-            ref Vector3<T> min,
-            ref Vector3<T> max,
-            out Vector3<T> result
-        )
-        {
-            throw new NotImplementedException();
-        }
-
-        public static T Distance(Vector3<T> vec1, Vector3<T> vec2)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Distance(ref Vector3<T> vec1, ref Vector3<T> vec2, out T result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static T DistanceSquared(Vector3<T> vec1, Vector3<T> vec2)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void DistanceSquared(ref Vector3<T> vec1, ref Vector3<T> vec2, out T result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Normalize(Vector3<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Normalize(ref Vector3<T> vec, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> NormalizeFast(Vector3<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void NormalizeFast(ref Vector3<T> vec, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static T Dot(Vector3<T> left, Vector3<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Dot(ref Vector3<T> left, ref Vector3<T> right, out T result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Cross(Vector3<T> left, Vector3<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Cross(ref Vector3<T> left, ref Vector3<T> right, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Lerp(Vector3<T> a, Vector3<T> b, T blend)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Lerp(ref Vector3<T> a, ref Vector3<T> b, T blend, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> BaryCentric(Vector3<T> a, Vector3<T> b, Vector3<T> c, T u, T v)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void BaryCentric
-        (
-            ref Vector3<T> a,
-            ref Vector3<T> b,
-            ref Vector3<T> c,
-            T u,
-            T v,
-            out Vector3<T> result
-        )
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> TransformVector(Vector3<T> vec, Matrix4<T> mat)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void TransformVector(ref Vector3<T> vec, ref Matrix4<T> mat, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> TransformNormal(Vector3<T> norm, Matrix4<T> mat)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void TransformNormal(ref Vector3<T> norm, ref Matrix4<T> mat, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> TransformNormalInverse(Vector3<T> norm, Matrix4<T> invMat)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void TransformNormalInverse
-        (
-            ref Vector3<T> norm,
-            ref Matrix4<T> invMat,
-            out Vector3<T> result
-        )
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> TransformPosition(Vector3<T> pos, Matrix4<T> mat)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void TransformPosition(ref Vector3<T> pos, ref Matrix4<T> mat, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Transform(Vector3<T> vec, Matrix3<T> mat)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Transform(ref Vector3<T> vec, ref Matrix3<T> mat, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Transform(Vector3<T> vec, Quaternion<T> quat)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Transform(ref Vector3<T> vec, ref Quaternion<T> quat, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Transform(Matrix3<T> mat, Vector3<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Transform(ref Matrix3<T> mat, ref Vector3<T> vec, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> TransformPerspective(Vector3<T> vec, Matrix4<T> mat)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void TransformPerspective(ref Vector3<T> vec, ref Matrix4<T> mat, out Vector3<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static T CalculateAngle(Vector3<T> first, Vector3<T> second)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void CalculateAngle(ref Vector3<T> first, ref Vector3<T> second, out T result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Project
-        (
-            Vector3<T> vector,
-            T x,
-            T y,
-            T width,
-            T height,
-            T minZ,
-            T maxZ,
-            Matrix4<T> worldViewProjection
-        )
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> Unproject
-        (
-            Vector3<T> vector,
-            T x,
-            T y,
-            T width,
-            T height,
-            T minZ,
-            T maxZ,
-            Matrix4<T> inverseWorldViewProjection
-        )
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> operator +(Vector3<T> left, Vector3<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> operator -(Vector3<T> left, Vector3<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> operator -(Vector3<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> operator *(Vector3<T> vec, T scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> operator *(T scale, Vector3<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> operator *(Vector3<T> vec, Vector3<T> scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> operator *(Vector3<T> vec, Matrix3<T> mat)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> operator *(Matrix3<T> mat, Vector3<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> operator *(Quaternion<T> quat, Vector3<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector3<T> operator /(Vector3<T> vec, T scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static bool operator ==(Vector3<T> left, Vector3<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static bool operator !=(Vector3<T> left, Vector3<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static implicit operator Vector3<T>((T X, T Y, T Z) values)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override string ToString()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int GetHashCode()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool Equals(object obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Equals(Vector3<T> other)
-        {
-            throw new NotImplementedException();
-        }
+            get
+            {
+                AssertInRange(nameof(index), index, 0, 3);
+                return index switch
+                {
+                    0 => X,
+                    1 => Y,
+                    2 => Z,
+                    _ => default
+                };
+            }
+            set
+            {
+                AssertInRange(nameof(index), index, 0, 3);
+                switch (index)
+                {
+                    case 0:
+                        X = value;
+                        break;
+                    case 1:
+                        Y = value;
+                        break;
+                    case 2:
+                        Z = value;
+                        break;
+                }
+            }
+        }
+
+        public readonly T GetLength() => Sqrt(GetLengthSquared());
+        public readonly T GetLengthSquared() => Add(Add(Multiply(X, X), Multiply(Y, Y)), Multiply(Z, Z));
+        public readonly Vector3<T> GetNormalized() => Vector3.Normalize(this);
+        public static Vector3<T> operator +(Vector3<T> left, Vector3<T> right) => Vector3.Add(left, right);
+        public static Vector3<T> operator -(Vector3<T> left, Vector3<T> right) => Vector3.Subtract(left, right);
+        public static Vector3<T> operator -(Vector3<T> vec) => Vector3.Negate(vec);
+        public static Vector3<T> operator *(Vector3<T> vec, T scale) => Vector3.Multiply(vec, scale);
+        public static Vector3<T> operator *(T scale, Vector3<T> vec) => Vector3.Multiply(vec, scale);
+        public static Vector3<T> operator *(Vector3<T> vec, Vector3<T> scale) => Vector3.Multiply(vec, scale);
+        public static Vector3<T> operator *(Vector3<T> vec, Matrix3<T> mat) => Vector3.Transform(vec, mat);
+        public static Vector3<T> operator *(Matrix3<T> mat, Vector3<T> vec) => Vector3.Transform(mat, vec);
+        public static Vector3<T> operator *(Quaternion<T> quat, Vector3<T> vec) => Vector3.Transform(vec, quat);
+        public static Vector3<T> operator /(Vector3<T> vec, T scale) => Vector3.Divide(vec, scale);
+        public static bool operator ==(Vector3<T> left, Vector3<T> right) => left.Equals(right);
+        public static bool operator !=(Vector3<T> left, Vector3<T> right) => !(left == right);
+        public static implicit operator Vector3<T>((T X, T Y, T Z) values) => new Vector3<T>(values.X, values.Y, values.Z);
+        public override string ToString() => $"({X}{ListSeparator} {Y}{ListSeparator} {Z})";
+        public override int GetHashCode() => HashCode.Combine(X, Y, Z);
+        public override bool Equals(object obj) => obj is Vector3<T> vec && Equals(vec);
+        public bool Equals(Vector3<T> other) => Equal(other.X, X) && Equal(other.Y, Y) && Equal(other.Z, Z);
 
         public void Deconstruct(out T x, out T y, out T z)
         {
-            throw new NotImplementedException();
+            x = X;
+            y = Y;
+            z = Z;
         }
 
 #if INTRINSICS
-        public System.Runtime.Intrinsics.Vector64<T> AsVector64()
-        {
-            throw new NotImplementedException();
-        }
+        public System.Runtime.Intrinsics.Vector64<T> AsVector64() => throw new NotImplementedException();
 
-        public System.Runtime.Intrinsics.Vector128<T> AsVector128()
-        {
-            throw new NotImplementedException();
-        }
+        public System.Runtime.Intrinsics.Vector128<T> AsVector128() => throw new NotImplementedException();
 
-        public System.Runtime.Intrinsics.Vector256<T> AsVector256()
-        {
-            throw new NotImplementedException();
-        }
+        public System.Runtime.Intrinsics.Vector256<T> AsVector256() => throw new NotImplementedException();
 #endif
 
 #if BTEC_INTRINSICS
-        public System.Numerics.Vector<T> AsVector()
-        {
-            throw new NotImplementedException();
-        }
+        public System.Numerics.Vector<T> AsVector() => throw new NotImplementedException();
 #endif
     }
 }
