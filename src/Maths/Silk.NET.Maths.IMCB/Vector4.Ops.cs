a5c26fea086843be3e549dc6757222f582b71cbe
Dylan Perks <dmp9biz@gmail.com>
2020-11-07 20:54:11 +0000

    Implement Vector4, add empty Vector4.Ops

---
+++ src/Maths/Silk.NET.Maths.IMCB/Vector4.Ops.cs
@@ -0,0 +1,114 @@
+﻿// This file is part of Silk.NET.
+// 
+// You may modify and distribute Silk.NET under the terms
+// of the MIT license. See the LICENSE file for details.
+
+using System;
+
+namespace Silk.NET.Maths
+{
+    public static class Vector4
+    {
+        public static Vector4<T> Add<T>(Vector4<T> a, Vector4<T> b) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> Subtract<T>(Vector4<T> a, Vector4<T> b) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> Multiply<T>(Vector4<T> vector, T scale) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> Multiply<T>(Vector4<T> vector, Vector4<T> scale) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> Divide<T>(Vector4<T> vector, T scale) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> Divide<T>(Vector4<T> vector, Vector4<T> scale) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> ComponentMin<T>(Vector4<T> a, Vector4<T> b) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> ComponentMax<T>(Vector4<T> a, Vector4<T> b) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> MagnitudeMin<T>(Vector4<T> left, Vector4<T> right) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> MagnitudeMax<T>(Vector4<T> left, Vector4<T> right) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> Clamp<T>(Vector4<T> vec, Vector4<T> min, Vector4<T> max) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> Normalize<T>(Vector4<T> vec) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static T Dot<T>(Vector4<T> left, Vector4<T> right) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> Lerp<T>(Vector4<T> a, Vector4<T> b, T blend) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> BaryCentric<T>(Vector4<T> a, Vector4<T> b, Vector4<T> c, T u, T v) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> Transform<T>(Vector4<T> vec, Matrix4<T> mat) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> TransformColumn<T>(Vector4<T> vec, Matrix4<T> mat) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> Transform<T>(Vector4<T> vec, Quaternion<T> quat) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> Transform<T>(Matrix4<T> mat, Vector4<T> vec) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vector4<T> TransformColumn<T>(Matrix4<T> mat, Vector4<T> vec) where T:unmanaged
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static Vector4<T> Negate<T>(Vector4<T> vec) where T:unmanaged => Multiply(vec, Vector4<T>.MinusOne);
+    }
+}
--- src/Maths/Silk.NET.Maths.IMCB/Vector4.cs
+++ src/Maths/Silk.NET.Maths.IMCB/Vector4.cs
@@ -5,22 +5,24 @@
 
 using System;
 using System.Runtime.CompilerServices;
+using static Silk.NET.Maths.Helper;
+using static Silk.NET.Maths.Scalar;
 
 namespace Silk.NET.Maths
 {
     [Serializable]
     public struct Vector4<T> : IEquatable<Vector4<T>> where T : unmanaged
     {
-        public static readonly Vector4<T> UnitX;
-        public static readonly Vector4<T> UnitY;
-        public static readonly Vector4<T> UnitZ;
-        public static readonly Vector4<T> UnitW;
-        public static readonly Vector4<T> Zero;
-        public static readonly Vector4<T> One;
-        public static readonly Vector4<T> PositiveInfinity;
-        public static readonly Vector4<T> NegativeInfinity;
-        public static readonly int SizeInBytes;
-        private static readonly string ListSeparator;
+        public static readonly Vector4<T> UnitX = new Vector4<T>(Scalar<T>.One, Scalar<T>.Zero, Scalar<T>.Zero, Scalar<T>.Zero);
+        public static readonly Vector4<T> UnitY = new Vector4<T>(Scalar<T>.Zero, Scalar<T>.One, Scalar<T>.Zero, Scalar<T>.Zero);
+        public static readonly Vector4<T> UnitZ = new Vector4<T>(Scalar<T>.Zero, Scalar<T>.Zero, Scalar<T>.One, Scalar<T>.Zero);
+        public static readonly Vector4<T> UnitW = new Vector4<T>(Scalar<T>.Zero, Scalar<T>.Zero, Scalar<T>.Zero, Scalar<T>.One);
+        public static readonly Vector4<T> Zero = new Vector4<T>(Scalar<T>.Zero);
+        public static readonly Vector4<T> One = new Vector4<T>(Scalar<T>.One);
+        public static readonly Vector4<T> MinusOne = new Vector4<T>(Negate(Scalar<T>.One));
+        public static readonly Vector4<T> PositiveInfinity = new Vector4<T>(Scalar<T>.PositiveInfinity);
+        public static readonly Vector4<T> NegativeInfinity = new Vector4<T>(Scalar<T>.NegativeInfinity);
+        public static readonly unsafe int SizeInBytes = sizeof(Vector4<T>);
         public T X;
         public T Y;
         public T Z;
@@ -28,380 +30,124 @@ namespace Silk.NET.Maths
 
         public Vector4(T value)
         {
-            throw new NotImplementedException();
+            X = value;
+            Y = value;
+            Z = value;
+            W = value;
         }
 
         public Vector4(T x, T y, T z, T w)
         {
-            throw new NotImplementedException();
+            X = x;
+            Y = y;
+            Z = z;
+            W = w;
         }
 
         public Vector4(Vector2<T> v)
         {
-            throw new NotImplementedException();
+            X = v.X;
+            Y = v.Y;
+            Z = Scalar<T>.Zero;
+            W = Scalar<T>.Zero;
         }
 
         public Vector4(Vector3<T> v)
         {
-            throw new NotImplementedException();
+            X = v.X;
+            Y = v.Y;
+            Z = v.Z;
+            W = Scalar<T>.Zero;
         }
 
         public Vector4(Vector3<T> v, T w)
         {
-            throw new NotImplementedException();
+            X = v.X;
+            Y = v.Y;
+            Z = v.Z;
+            W = w;
         }
 
         public Vector4(Vector4<T> v)
         {
-            throw new NotImplementedException();
+            X = v.X;
+            Y = v.Y;
+            Z = v.Z;
+            W = v.W;
         }
 
         public T this[int index]
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            get
+            {
+                AssertInRange(nameof(index), index, 0, 4);
+                return index switch
+                {
+                    0 => X,
+                    1 => Y,
+                    2 => Z,
+                    3 => W,
+                    _ => default
+                };
+            }
+            set
+            {
+                AssertInRange(nameof(index), index, 0, 4);
+                switch (index)
+                {
+                    case 0:
+                        X = value;
+                        break;
+                    case 1:
+                        Y = value;
+                        break;
+                    case 2:
+                        Z = value;
+                        break;
+                    case 3:
+                        W = value;
+                        break;
+                }
+            }
         }
 
-        public T Length => throw new NotImplementedException();
-        public T LengthFast => throw new NotImplementedException();
-        public T LengthSquared => throw new NotImplementedException();
-
-        public Vector4<T> Normalized()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Normalize()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void NormalizeFast()
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> Add(Vector4<T> a, Vector4<T> b)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Add(ref Vector4<T> a, ref Vector4<T> b, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> Subtract(Vector4<T> a, Vector4<T> b)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Subtract(ref Vector4<T> a, ref Vector4<T> b, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> Multiply(Vector4<T> vector, T scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Multiply(ref Vector4<T> vector, T scale, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> Multiply(Vector4<T> vector, Vector4<T> scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Multiply(ref Vector4<T> vector, ref Vector4<T> scale, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> Divide(Vector4<T> vector, T scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Divide(ref Vector4<T> vector, T scale, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> Divide(Vector4<T> vector, Vector4<T> scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Divide(ref Vector4<T> vector, ref Vector4<T> scale, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> ComponentMin(Vector4<T> a, Vector4<T> b)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void ComponentMin(ref Vector4<T> a, ref Vector4<T> b, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> ComponentMax(Vector4<T> a, Vector4<T> b)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void ComponentMax(ref Vector4<T> a, ref Vector4<T> b, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> MagnitudeMin(Vector4<T> left, Vector4<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void MagnitudeMin(ref Vector4<T> left, ref Vector4<T> right, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> MagnitudeMax(Vector4<T> left, Vector4<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void MagnitudeMax(ref Vector4<T> left, ref Vector4<T> right, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> Clamp(Vector4<T> vec, Vector4<T> min, Vector4<T> max)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Clamp
-        (
-            ref Vector4<T> vec,
-            ref Vector4<T> min,
-            ref Vector4<T> max,
-            out Vector4<T> result
-        )
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> Normalize(Vector4<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Normalize(ref Vector4<T> vec, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> NormalizeFast(Vector4<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void NormalizeFast(ref Vector4<T> vec, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static T Dot(Vector4<T> left, Vector4<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Dot(ref Vector4<T> left, ref Vector4<T> right, out T result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> Lerp(Vector4<T> a, Vector4<T> b, T blend)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Lerp(ref Vector4<T> a, ref Vector4<T> b, T blend, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> BaryCentric(Vector4<T> a, Vector4<T> b, Vector4<T> c, T u, T v)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void BaryCentric
-        (
-            ref Vector4<T> a,
-            ref Vector4<T> b,
-            ref Vector4<T> c,
-            T u,
-            T v,
-            out Vector4<T> result
-        )
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> Transform(Vector4<T> vec, Matrix4<T> mat)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Transform(ref Vector4<T> vec, ref Matrix4<T> mat, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> Transform(Vector4<T> vec, Quaternion<T> quat)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Transform(ref Vector4<T> vec, ref Quaternion<T> quat, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> Transform(Matrix4<T> mat, Vector4<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void Transform(ref Matrix4<T> mat, ref Vector4<T> vec, out Vector4<T> result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> operator +(Vector4<T> left, Vector4<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> operator -(Vector4<T> left, Vector4<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> operator -(Vector4<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> operator *(Vector4<T> vec, T scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> operator *(T scale, Vector4<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> operator *(Vector4<T> vec, Vector4<T> scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> operator *(Vector4<T> vec, Matrix4<T> mat)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> operator *(Matrix4<T> mat, Vector4<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> operator *(Quaternion<T> quat, Vector4<T> vec)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Vector4<T> operator /(Vector4<T> vec, T scale)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static bool operator ==(Vector4<T> left, Vector4<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static bool operator !=(Vector4<T> left, Vector4<T> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static explicit operator IntPtr(Vector4<T> v)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static implicit operator Vector4<T>((T X, T Y, T Z, T W) values)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override string ToString()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int GetHashCode()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool Equals(object obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Equals(Vector4<T> other)
-        {
-            throw new NotImplementedException();
-        }
+        public T GetLength() => Sqrt(GetLengthSquared());
+        public T GetLengthSquared() => Add(Add(Add(Multiply(X, X), Multiply(Y, Y)), Multiply(Z, Z)), Multiply(W, W));
+        public Vector4<T> GetNormalized() => Vector4.Normalize(this);
+        public static Vector4<T> operator +(Vector4<T> left, Vector4<T> right) => Vector4.Add(left, right);
+        public static Vector4<T> operator -(Vector4<T> left, Vector4<T> right) => Vector4.Subtract(left, right);
+        public static Vector4<T> operator -(Vector4<T> vec) => Vector4.Negate(vec);
+        public static Vector4<T> operator *(Vector4<T> vec, T scale) => Vector4.Multiply(vec, scale);
+        public static Vector4<T> operator *(T scale, Vector4<T> vec) => Vector4.Multiply(vec, scale);
+        public static Vector4<T> operator *(Vector4<T> vec, Vector4<T> scale) => Vector4.Multiply(vec, scale);
+        public static Vector4<T> operator *(Vector4<T> vec, Matrix4<T> mat) => Vector4.Transform(vec, mat);
+        public static Vector4<T> operator *(Matrix4<T> mat, Vector4<T> vec) => Vector4.Transform(mat, vec);
+        public static Vector4<T> operator *(Quaternion<T> quat, Vector4<T> vec) => Vector4.Transform(vec, quat);
+        public static Vector4<T> operator /(Vector4<T> vec, T scale) => Vector4.Divide(vec, scale);
+        public static bool operator ==(Vector4<T> left, Vector4<T> right) => left.Equals(right);
+        public static bool operator !=(Vector4<T> left, Vector4<T> right) => !(left == right);
+        public static implicit operator Vector4<T>((T X, T Y, T Z, T W) values) => new Vector4<T>(values.X, values.Y, values.Z, values.W);
+        public override string ToString() => $"({X}{ListSeparator} {Y}{ListSeparator} {Z}{ListSeparator} {W})";
+        public override int GetHashCode() => HashCode.Combine(X, Y, Z, W);
+        public override bool Equals(object obj) => obj is Vector4<T> vec && Equals(vec);
+        public bool Equals(Vector4<T> other) => Equal(X, other.X) && Equal(Y, other.Y) && Equal(Z, other.Z) && Equal(W, other.W);
 
         public void Deconstruct(out T x, out T y, out T z, out T w)
         {
-            throw new NotImplementedException();
+            x = X;
+            y = Y;
+            z = Z;
+            w = W;
         }
 
 #if INTRINSICS
-        public System.Runtime.Intrinsics.Vector64<T> AsVector64()
-        {
-            throw new NotImplementedException();
-        }
-
-        public System.Runtime.Intrinsics.Vector128<T> AsVector128()
-        {
-            throw new NotImplementedException();
-        }
-
-        public System.Runtime.Intrinsics.Vector256<T> AsVector256()
-        {
-            throw new NotImplementedException();
-        }
+        public System.Runtime.Intrinsics.Vector64<T> AsVector64() => throw new NotImplementedException();
+        public System.Runtime.Intrinsics.Vector128<T> AsVector128() => throw new NotImplementedException();
+        public System.Runtime.Intrinsics.Vector256<T> AsVector256() => throw new NotImplementedException();
 #endif
 
 #if BTEC_INTRINSICS
-        public System.Numerics.Vector<T> AsVector()
-        {
-            throw new NotImplementedException();
-        }
+        public System.Numerics.Vector<T> AsVector() => throw new NotImplementedException();
 #endif
     }
 }
